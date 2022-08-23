e0ad1d71205f89e343c15962498d87613b2a92a9
Jason <Jasonvspace@gmail.com>
2022-08-22 20:42:23 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">
+        Button as link
+      </Button>
+    </div>
+  );
+}
+
+export default App;
